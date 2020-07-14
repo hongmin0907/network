3467589058c590ab1df67f49f166bc37a556e5ea
Hong min Son <ghdals0907@naver.com>
2020-07-15 05:15:11 +0900

    tcp and web basic exam

---
+++ Web_exam/browser/main.cpp
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+#ifndef __linux__
+#include <WinSock2.h>
+#include <Windows.h>
+#define perror(msg) winperror(msg)
+
+void winperror(const char* msg);
+#else
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
+int main() {
+	int sock;
+	struct sockaddr_in addr;
+	char buffer[1024];
+	int len;
+	const char* http_request = "GET / HTTP/1.0\r\n\r\n";
+
+#ifndef __linux__
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+#endif
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		return 1;
+	}
+
+	memset(&addr, 0x00, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr("218.144.131.23");
+	addr.sin_port = htons(80);
+
+	if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+		perror("connect");
+		return 1;
+	}
+
+	if (send(sock, http_request, strlen(http_request), 0) < 0) {
+		perror("send");
+		return 1;
+	}
+
+	while ((len = recv(sock, buffer, 1024, 0)) > 0) {
+		buffer[len] = '\0';
+		printf("%s", buffer);
+	}
+
+#ifndef __linux__
+	closesocket(sock);
+	WSACleanup();
+#else
+	close(sock);
+#endif
+
+	return 0;
+}
+
+#ifndef __linux__
+void winperror(const char* msg) {
+	LPVOID lpMsgBuf;
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+
+	printf("%s : %s \n", msg, lpMsgBuf);
+
+	LocalFree(lpMsgBuf);
+}
+#endif
