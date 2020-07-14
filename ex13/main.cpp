0799b1ab498cbfa408c2b8ae7801ca8446f20c0a
Hong min Son <ghdals0907@naver.com>
2020-07-14 21:31:14 +0900

    first push example

---
+++ ex13/main.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main() {
+	printf("errno: %d\n", errno);
+	printf("malloc: %s\n", strerror(errno));
+
+	return 0;
+}
